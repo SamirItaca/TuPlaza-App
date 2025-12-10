41c20fdc6ed2dbee4ce7988e111ff3d7f935c431
Jonnatan <0423jhenao@e-itaca.es>
2025-12-10 19:20:16 +0100

    añadi las clases a los inputs y selects

--- frontend/tuplaza-angular/src/app/features/home/pages/home/home.ts
+++ frontend/tuplaza-angular/src/app/features/home/pages/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../../../shared/button/button.component';
+import { FormularioComponent } from '../../../../shared/formulario/formulario.component';
 
 @Component({
   selector: 'app-home',
-  imports: [ButtonComponent],
+  imports: [ButtonComponent,FormularioComponent],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
