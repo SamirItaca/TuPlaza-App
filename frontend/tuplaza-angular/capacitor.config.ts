bef286c164ffe75fe78aab21dee6f7ae74c00f62
Samir <hexagonalsamir@gmail.com>
2025-12-15 11:45:31 +0100

    integrando ionic y capacitor, tambien ios con xcode

---
+++ frontend/tuplaza-angular/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tuplaza.app',
+  appName: 'tuplaza-angular',
+  webDir: 'dist/tuplaza-angular/browser'
+};
+
+export default config;
