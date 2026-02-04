ebb7c4718a003977ab26c4059ed1864c784cc4ab
Samir <hexagonalsamir@gmail.com>
2026-02-04 18:42:10 +0100

    estilo de tab listo

--- frontend/tuplaza-angular/src/app/shared/tab/tab.component.spec.ts
+++ frontend/tuplaza-angular/src/app/shared/tab/tab.component.spec.ts
@@ -21,4 +21,24 @@ describe('TabComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should initialize tabButtons with correct pages on ngOnInit', () => {
+    component.ngOnInit();
+    expect(component.tabButtons.length).toBe(5);
+
+    expect(component.tabButtons).toEqual([
+      { tab: 'home', iconName: 'home', text: 'Home' },
+      { tab: 'garajes', iconName: 'car', text: 'Garajes' },
+      { tab: 'favoritos', iconName: 'heart', text: 'Favoritos' },
+      { tab: 'publicar', iconName: 'add-circle', text: 'Publicar' },
+      { tab: 'mis-garajes', iconName: 'folder-open', text: 'Mis garajes' }
+    ]);
+  });
+
+  it('initPagesInTabButton should populate tabButtons correctly', () => {
+    component.ngOnInit();  // inicializa tabButtons
+    expect(component.tabButtons.length).toBe(5);
+    expect(component.tabButtons[0].tab).toBe('home');
+  });
+
 });
