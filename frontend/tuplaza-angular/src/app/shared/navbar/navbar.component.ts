0137d6c25b9638880d0facf55524c078fb3ebb8a
Samir <hexagonalsamir@gmail.com>
2026-02-05 13:14:24 +0100

    estilo del navbar listo

--- frontend/tuplaza-angular/src/app/shared/navbar/navbar.component.ts
+++ frontend/tuplaza-angular/src/app/shared/navbar/navbar.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { IonHeader, IonToolbar, IonItem, IonInput, IonButtons, IonMenuButton } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonItem, IonIcon, IonInput, IonButtons, IonMenuButton } from '@ionic/angular/standalone';
+
+import { addIcons } from 'ionicons';
+import { searchOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'tuplaza-nav',
-  imports: [RouterModule, IonHeader, IonToolbar, IonItem, IonInput, IonButtons, IonMenuButton],
+  imports: [RouterModule, IonHeader, IonToolbar, IonItem, IonIcon, IonInput, IonButtons, IonMenuButton],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
   
+  constructor() { 
+    addIcons({ searchOutline });
+  }
+
 }
