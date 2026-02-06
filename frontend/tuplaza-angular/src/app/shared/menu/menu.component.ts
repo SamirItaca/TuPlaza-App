f88fcc7a5a27827c859a41f71d7b26cd7a1fbf27
Samir <hexagonalsamir@gmail.com>
2026-02-06 01:29:28 +0100

    estilo del menu listo

--- frontend/tuplaza-angular/src/app/shared/menu/menu.component.ts
+++ frontend/tuplaza-angular/src/app/shared/menu/menu.component.ts
@@ -7,18 +7,25 @@ import {
   IonTitle,
   IonContent,
   IonList,
-  IonItem
+  IonItem,
+  IonIcon,
+  IonLabel
 } from '@ionic/angular/standalone';
+
+import { addIcons } from 'ionicons';
+import { personOutline, settingsOutline, alertCircleOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'tuplaza-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss'],
-  imports: [IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem]
+  imports: [IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonIcon, IonLabel]
 })
-export class MenuComponent  implements OnInit {
+export class MenuComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    addIcons({ personOutline, settingsOutline, alertCircleOutline });
+  }
 
   ngOnInit() {}
 
