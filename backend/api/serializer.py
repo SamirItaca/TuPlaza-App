0f445c2e457f82e1ff2037291c15e91bdbad43fe
Jonnatan Henao <j.henao@aragonphotonics.com>
2026-03-11 15:35:52 +0100

    Serializador creado para las comunicaciones entre angular y django

---
+++ backend/api/serializer.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import Usuario, Garaje, Reserva
+
+class UsuarioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Usuario
+        fields = ['id', 'nombre', 'email', 'telefono', 'verificado'] 
+
+class GarajeSerializer(serializers.ModelSerializer):
+    # Esta línea permite que Angular reciba los datos del dueño, no solo su ID
+    propietario_detalle = UsuarioSerializer(source='propietario', read_only=True)
+    
+    class Meta:
+        model = Garaje
+        fields = [
+            'id', 'descripcion', 'precio', 'disponible', 
+            'propietario', 'propietario_detalle'
+        ] 
